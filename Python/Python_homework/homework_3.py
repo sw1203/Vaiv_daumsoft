4a96b430ededbb6822285918152890c8d7df9d52
sw1203 <tkddnjs1203@gmail.com>
2020-10-12 17:00:23 +0900

    Change filename

--- Python/Python_homework/homework_3.py
+++ Python/Python_homework/homework_3.py
@@ -1,37 +1,26 @@
 import json
-import gzip
-"""
-goodreads_reviews_dedup.json.gz 파일은 https://sites.google.com/eng.ucsd.edu/ucsdbookgraph/reviews에서 다운
-"""
 
 
-def read_lines():
-    with gzip.open("goodreads_reviews_dedup.json.gz", 'r') as file:
+def solution(json_file):
+    answer = {}
+    with open(json_file, 'r') as file:
         while True:
             line = file.readline()
             if not line:
-                return 0
+                break
+            review = json.loads(line)
+            if review['book_id'] not in answer:
+                answer[review['book_id']] = [0 for i in range(6)]
+                answer[review['book_id']][review['rating']] += 1
             else:
-                yield line
+                answer[review['book_id']][review['rating']] += 1
 
+    answer = sorted(answer.items(), key=lambda x: int(x[0]))
 
-text = read_lines()
-answer = {}
-while True:
-    try:
-        review = json.loads(next(text))
-        if review['book_id'] not in answer:
-            answer[review['book_id']] = [0 for i in range(6)]
-            answer[review['book_id']][review['rating']] += 1
-        else:
-            answer[review['book_id']][review['rating']] += 1
-    except StopIteration:
-        break
+    with open("answer_2.txt", 'w', encoding='UTF-8') as write_file:
+        for book_id, rating in answer:
+            write_file.write(f"{book_id} {rating[0]} {rating[1]} {rating[2]} {rating[3]} {rating[4]} {rating[5]}\n")
 
-answer = sorted(answer.items(), key=lambda x: int(x[0]))
+    print(f"{len(answer)} end")
 
-with open("answer.txt", 'w', encoding='UTF-8') as write_file:
-    for book_id, rating in answer:
-        write_file.write(f"{book_id} {rating[0]} {rating[1]} {rating[2]} {rating[3]} {rating[4]} {rating[5]}\n")
-
-print(f"{len(answer)} end")
+solution("goodreads_reviews_dedup.json")
